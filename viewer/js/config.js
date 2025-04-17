29f4e245950e672e385f165f8a5d5e03ac35e142
Dimitris Nicoloutsopoulos <dimitris.nicoloutsopoulos@gmail.com>
2025-04-17 09:59:13 +0100

    fetching from gcs

--- viewer/js/config.js
+++ viewer/js/config.js
@@ -32,9 +32,9 @@
 //
 
  function config() { return{
-  "cellData": {"mediaLink": "../../data/cellData_filtered_shifted.tsv", "size": "16166143"},
-  "geneData": {"mediaLink": "../../data/geneData_filtered_shifted.tsv", "size": "272965691 "},
-  "cellBoundaries": {"mediaLink": "../../data/cellBoundaries_filtered_shifted.tsv", "size": "4025461"},
+  "cellData": {"mediaLink": "https://storage.googleapis.com/aang_data/cellData_filtered_shifted.tsv", "size": "6066619 "},
+  "geneData": {"mediaLink": "https://storage.googleapis.com/aang_data/geneData_filtered_shifted.tsv", "size": "22370911"},
+  "cellBoundaries": {"mediaLink": "https://storage.googleapis.com/aang_data/cellBoundaries_filtered_shifted.tsv", "size": "1189385"},
   "roi": {"x0": 0, "x1": 6431, "y0": 0, "y1": 8543}, "maxZoom": 8,
   "layers": {
    // "empty": "",
