699899bba4186bce215def2f7a442d1eb809ff66
Dimitris Nicoloutsopoulos <dimitris.nicoloutsopoulos@gmail.com>
2026-08-25 20:57:19 +0100

    silver dataset on config.js

--- config.js
+++ config.js
@@ -7,11 +7,11 @@ function config() {
     return {
 
         // What are the dimensions of your images? (in pixels)
-        imageWidth: 2304,
-        imageHeight: 2304,
+        imageWidth: 6411,
+        imageHeight: 4412,
 
         // Size of a cubic pixel in microns (x, y, z)
-        voxelSize: [1, 1, 3.46],
+        voxelSize: [0.28, 0.28, 0.7],
 
         // Background image channels (base layers).
         // Each channel is one selectable background. The first channel is shown
@@ -21,30 +21,30 @@ function config() {
         // Optional "tint" ("#RRGGBB") colours a grayscale channel, e.g. red GCaMP;
         // a channel with no tint renders exactly as its source image.
         channels: [
-            // {
-            //     id: "hc",
-            //     label: "HC",
-            //     tiles: "https://storage.googleapis.com/christina_silver_hc/tiles_hc/tiles_{plane}/{z}/{y}/{x}.jpg"
-            // }
-            // Add more channels to enable the basemap switcher, for example:
             {
-                id: "dapi",
-                label: "DAPI",
-                tiles: "https://storage.googleapis.com/maxs_data/BZ008_S10_tile10/tiles/S10_tile10_dapi/plane_{plane}/{z}/{y}/{x}.jpg"
-            },
-            {
-                id: "gcamp",
-                label: "GCaMP",
-                tiles: "https://storage.googleapis.com/maxs_data/BZ008_S10_tile10/tiles/S10_tile10_gcamp/plane_{plane}/{z}/{y}/{x}.jpg",
-                tint: "00ff00"
+                id: "hc",
+                label: "HC",
+                tiles: "https://storage.googleapis.com/christina_silver_hc/tiles_hc/tiles_{plane}/{z}/{y}/{x}.jpg",
             }
+            // // Add more channels to enable the basemap switcher, for example:
+            // {
+            //     id: "dapi",
+            //     label: "DAPI",
+            //     tiles: "https://storage.googleapis.com/maxs_data/BZ008_S10_tile10/tiles/S10_tile10_dapi/plane_{plane}/{z}/{y}/{x}.jpg"
+            // },
+            // {
+            //     id: "gcamp",
+            //     label: "GCaMP",
+            //     tiles: "https://storage.googleapis.com/maxs_data/BZ008_S10_tile10/tiles/S10_tile10_gcamp/plane_{plane}/{z}/{y}/{x}.jpg",
+            //     tint: "00ff00"
+            // }
         ],
 
         // Arrow manifests
-        arrowSpotsManifest: "https://storage.googleapis.com/maxs_data/BZ008_S10_tile10/data/viewer_data/arrow_spots/manifest.json",
-        arrowCellsManifest: "https://storage.googleapis.com/maxs_data/BZ008_S10_tile10/data/viewer_data/arrow_cells/manifest.json",
-        arrowBoundariesManifest: "https://storage.googleapis.com/maxs_data/BZ008_S10_tile10/data/viewer_data/arrow_boundaries/manifest.json",
-        arrowSpotsGeneDict: "https://storage.googleapis.com/maxs_data/BZ008_S10_tile10/data/viewer_data/arrow_spots/gene_dict.json"
+        arrowSpotsManifest: "https://storage.googleapis.com/arrow_files/pciSeq_with_theta/rTheta_10/arrow_spots/manifest.json",
+        arrowCellsManifest: "https://storage.googleapis.com/arrow_files/pciSeq_with_theta/rTheta_10/arrow_cells/manifest.json",
+        arrowBoundariesManifest: "https://storage.googleapis.com/arrow_files/pciSeq_with_theta/rTheta_10/arrow_boundaries/manifest.json",
+        arrowSpotsGeneDict: "https://storage.googleapis.com/arrow_files/pciSeq_with_theta/rTheta_10/arrow_spots/gene_dict.json"
     };
 }
 
